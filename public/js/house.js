2fd62f2d5b5ef2f22b41cba7c202f403d43615d9
Charles Sun <2209339564@qq.com>
2022-10-05 21:42:06 +0800

    change the async logic of adding house

--- public/js/house.js
+++ public/js/house.js
@@ -4,43 +4,43 @@ const form2 = [...document.querySelector('.forms2').children];
 form1.forEach((item, i) => {
     setTimeout(() => {
         item.style.opacity = 1;
-    }, i*100);
+    }, i * 100);
 })
 
 form2.forEach((item, i) => {
     setTimeout(() => {
         item.style.opacity = 1;
-    }, i*100);
+    }, i * 100);
 })
 
 
 
 window.onload = () => {
-    if(!sessionStorage.name){
+    if (!sessionStorage.name) {
         location.href = '/';
-    }else{
+    } else {
         document.title = `hello ${sessionStorage.name}`;
-          fetch('/house-get', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                  email: sessionStorage.email
-              })
-          })
-          .then(res => res.json())
-          .then(data => {
-              // update the house list, use vue
-              data=data.map(item => {
-                return {
-                  'House Name': item.h_name,
-                  'House Type': type_id_to_name(item.h_type)
-                }
-              })
-              console.log(data);
-              vm.gridData=data;
-          })
+        fetch('/house-get', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: sessionStorage.email
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                // update the house list, use vue
+                data = data.map(item => {
+                    return {
+                        'House Name': item.h_name,
+                        'House Type': type_id_to_name(item.h_type)
+                    }
+                })
+                console.log(data);
+                vm.gridData = data;
+            })
     }
 }
 
@@ -48,19 +48,40 @@ window.onload = () => {
 const createHouse = document.querySelector('.submit-btn');
 createHouse.onclick = () => {
     // post a request to create a house
-    console.log('clicked');
-    console.log(sessionStorage.email);
     fetch('/house', {
         method: 'POST',
-        headers: new Headers({'Content-Type': 'application/json'}),
+        headers: new Headers({ 'Content-Type': 'application/json' }),
         body: JSON.stringify({
             email: sessionStorage.email,
-            h_name: document.querySelector('.h_name').value==''?'default':document.querySelector('.h_name').value,
+            h_name: document.querySelector('.h_name').value == '' ? 'default' : document.querySelector('.h_name').value,
             h_type: document.querySelector('input[name="h_type"]:checked').value
         })
     })
-    .then(res => res.json())
-    .then(data => {
-        alertBox(data.name);
-    })
+        .then(res => res.json())
+        .then(data => {
+            alertBox(data.name);
+        }).then(() => {
+
+            fetch('/house-get', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: sessionStorage.email
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // update the house list, use vue
+                    data = data.map(item => {
+                        return {
+                            'House Name': item.h_name,
+                            'House Type': type_id_to_name(item.h_type)
+                        }
+                    })
+                    console.log(data);
+                    vm.gridData = data;
+                })
+        })
 }
